59147c1fc748b92c34b1076fb9ae041927adc846
Hadar Sharvit <hadar.sharvit1@mail.huji.ac.il>
2021-06-25 01:39:03 +0300

    fixed some errors, left the code with compilation errors

--- ex5/sol5.py
+++ ex5/sol5.py
@@ -6,13 +6,16 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import mean_squared_error
 
+"""
+initializing some variables here:
+"""
 m = 1500  # num samples
 X = np.random.uniform(-3.2, 2.2, m)
 f = lambda x: (x + 3) * (x + 2) * (x + 1) * (x - 1) * (x - 2)
 mu, sigma = 0, 1
 epsilon = np.random.normal(mu, sigma, m)  # noise
 y = f(X) + epsilon  # labels with noise
-poly_deg = 15
+poly_deg = 15  # maximal degree of polynomial
 
 
 def fit_polynomial(x_S, y_S, deg):
@@ -28,7 +31,38 @@ def fit_polynomial(x_S, y_S, deg):
     return h_S
 
 
-def plot_kfold(kfold, x_S, y_S, x_V, y_V):
+def set_2fold_data(x_D=X[:1000], y_D=y[:1000]):
+    """
+    separates the data to S,V,T for 2-fold
+    """
+    x_T, y_T = X[1000:], y[1000:]  # T for Test
+    x_S, y_S = x_D[:500], y_D[:500]  # S for train
+    x_V, y_V = x_D[500:], y_D[500:]  # V for validation
+    return x_D, y_D, x_T, y_T, x_S, y_S, x_V, y_V
+
+
+def plot_2fold_error():
+    x_D, y_D, x_T, y_T, x_S, y_S, x_V, y_V = set_2fold_data()
+    h_s = [fit_polynomial(x_S, y_S, d + 1) for d in range(poly_deg)]
+    y_hat_V_arr = [h_s[d].predict(x_V.reshape(-1, 1)) for d in range(poly_deg)]
+    V_err = [mean_squared_error(y_hat_V, y_V) for y_hat_V in y_hat_V_arr]
+    plt.plot(range(1, poly_deg + 1), V_err)
+    best_deg = np.argmin(V_err) + 1
+    plt.plot(range(1, poly_deg + 1), V_err, label="Validation Error")
+    plt.title(f"Fitting degrees of polynomial - best fit deg = {best_deg}")
+    plt.legend(), plt.xlabel("degree-k"), plt.ylabel("MSE error")
+    plt.grid(), plt.show()
+
+
+def set_kfold_data(k_fold=5, x_D=X[:1000], y_D=y[:1000]):
+    """
+    separates the data to S,V,T for k-fold, initialized with 5
+    """
+    seperator = np.remainder(np.arange(x_D.size), k_fold)
+    # x_S_fold = x_D[seperator !=]
+
+
+def kfold_error(kfold):
     """
     generates a list of validation error and train error for a given kFold parameter
     """
@@ -40,28 +74,20 @@ def plot_kfold(kfold, x_S, y_S, x_V, y_V):
         y_hat_S_arr = [h_s[d].predict(x_S.reshape(-1, 1)) for d in range(poly_deg)]
         V_err = [mean_squared_error(y_hat_V, y_V) for y_hat_V in y_hat_V_arr]
         S_err = [mean_squared_error(y_hat_S, y_S) for y_hat_S in y_hat_S_arr]
-
         valid_err += np.array(V_err) / kfold  # adding the respective normalized error for every d
         train_err += np.array(S_err) / kfold  # same here, just for S
     return valid_err, train_err
 
 
 def plot_errors(valid_err, train_err):
-    best_deg = np.argmin(valid_err) + 1
     print(f"valid={valid_err}")
     print(f"train={train_err}")
     plt.plot(range(1, poly_deg + 1), valid_err, label="Validation Error")
     plt.plot(range(1, poly_deg + 1), train_err, label="Training Error")
-    plt.title(f"Fitting degrees of polynomial - best fit ={best_deg}")
+    plt.title(f"Fitting degrees of polynomial")
     plt.legend(), plt.xlabel("degree-k"), plt.ylabel("MSE error")
     plt.grid(), plt.show()
 
 
 if __name__ == '__main__':
-    # 2-fold data:
-    x_D, y_D = X[:1000], y[:1000]  # D for S and V
-    x_T, y_T = X[1000:], y[1000:]  # T for Test
-    x_S, y_S = x_D[:500], y_D[:500]  # S for train
-    x_V, y_V = x_D[500:], y_D[500:]  # V for validatio
-    v_err, t_err = plot_kfold(2, x_S, y_S, x_V, y_V)
-    plot_errors(v_err, t_err)
+    plot_2fold_error()
